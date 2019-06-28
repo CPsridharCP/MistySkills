20edbb1ead5e2a90eb86f4e02c1f589afb7cafb8
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-06-27 18:45:22 -0600

    Add files via upload

---
+++ ExampleSkills/Advanced/moveToSound/moveToSound.js
@@ -0,0 +1,172 @@
+// Misty suttly looks at people who are speaking around her !!
+// It is intended to be slow and not super responsive - Feel free to play with latencey and make it faster 
+
+misty.ChangeLED(0, 0, 255);
+misty.Set("lookedAt",(new Date()).toUTCString());
+misty.MoveHeadDegrees(-15.0, 0.0, 0.0, 70);
+misty.StartRecordingAudio("deleteThis.wav");
+misty.Pause(4000);
+misty.ChangeLED(0, 255, 0);
+
+
+// Setting some global variables as normal var <variable_name> cannot be accessed in event callbacks
+misty.Set("headYaw", 0.0); 
+misty.Set("robotYaw", 0.0); 
+
+// Used for smoothening data - barely helps :D - Gotta do more layers for better softening
+misty.Set("1b", 0.0);
+misty.Set("2b",0.0);
+
+misty.Set("inProgress", false);
+
+registerAudioLocalisation();
+registerIMU();
+registerActuatorPosition();
+
+
+// -------------------------------------- Audio Localization --------------------------------------------------
+
+function registerAudioLocalisation(){
+    misty.AddReturnProperty("sound", "DegreeOfArrivalSpeech");
+    misty.RegisterEvent("sound", "SourceTrackDataMessage", 100 ,true); 
+}
+
+function _sound(data)
+{   
+    // We get the location of the Audio Speech in 0-360 degrees wrt Local Frame of Head
+    // Using the function toRobotFrame() it is mapped to Robots local frame
+    var vector = 0.4*toRobotFrame(data.AdditionalResults[0]) + 0.35*misty.Get("1b") + 0.25*misty.Get("2b") ;
+    // misty.Debug(data.AdditionalResults[0]);
+    if (!misty.Get("inProgress")) misty.Debug(vector+" <-- soundIn");
+
+    if (secondsPast(misty.Get("lookedAt"))>5.0 && !misty.Get("inProgress"))
+    {   
+        misty.UnregisterEvent("sound");   
+        misty.Set("inProgress", true);
+        misty.Debug(vector.toString()+" <-- Look At Input Global");
+        // misty.Pause(300);
+        lookAt(vector,misty.Get("robotYaw"),misty.Get("headYaw"));
+        registerAudioLocalisation();
+    }
+
+    misty.Set("2b",misty.Get("1b"));
+    misty.Set("1b",vector);
+}
+
+// -------------------------------------- Robot Control - MAIN -----------------------------------------------
+
+function lookAt(heading,robotYawAtStart,headYawAtStart)
+{
+    misty.Set("inProgress", true);
+    
+    // Head Motion 
+    var rawFinalHeadPose = misty.Get("headYaw")+(heading/2.0);
+    var actuateTo = rawFinalHeadPose;
+    actuateTo = actuateTo <-45.0 ? -45.0 : actuateTo;
+    actuateTo = actuateTo >45 ? 45.0 : actuateTo;
+    misty.Debug(actuateTo.toString()+" <- Head to move to");
+    misty.MoveHeadDegrees(-15.0, 0.5, actuateTo, 500);
+    
+    
+    // Body Motion
+    if (Math.abs(rawFinalHeadPose)>=45)
+    {   
+        misty.Pause(1000);
+        var globalHeading = offset_heading(heading+(headYawAtStart*2.0));
+        if (globalHeading>180) globalHeading-=360;
+        AngleDifference(robotYawAtStart,globalHeading)>=0 ? misty.Drive(0, 30) : misty.Drive(0, -30);// use speed 30
+        // misty.MoveHeadDegrees(-14.0, 0.0, 0.0,5);
+        misty.Debug(globalHeading.toString()+" <- Body to move to");
+        var initialError = Math.abs(AngleDifference(robotYawAtStart,globalHeading));
+        var currentAbsError = initialError;
+        var headResetDone = false;
+        while (Math.abs(misty.Get("robotYaw")-globalHeading)>=10)
+        { 
+            // misty.Debug(currentAbsError/initialError);
+            currentAbsError = Math.abs(AngleDifference(misty.Get("robotYaw"),globalHeading));
+            if (currentAbsError/initialError <0.70 && !headResetDone)
+            {
+                headResetDone = true;
+                misty.MoveHeadDegrees(-15.0, 0.0, 0.0,500);
+            }
+            else {}
+            misty.Pause(10);
+            // misty.Debug(misty.Get("robotYaw").toString()+" , " + globalHeading.toString());
+        }
+        // misty.Drive(0, 0);
+        misty.Stop();
+    } else {
+        //misty.Pause(3000);
+    }
+    misty.Debug("DONE");
+    misty.Set("lookedAt",(new Date()).toUTCString());
+    misty.Set("inProgress", false);
+}
+
+// ------------------------------------ Head Current Position Updater --------------------------------------
+
+function registerActuatorPosition()
+{   
+    misty.AddReturnProperty("Positions", "SensorId");
+    misty.AddReturnProperty("Positions", "Value");
+    misty.AddPropertyTest("Positions", "SensorId", "==", "ahy", "string"); //////////// ahy //////////
+    misty.RegisterEvent("Positions", "ActuatorPosition", 100 ,true); 
+}
+
+function _Positions(data) 
+{   
+    if (data.AdditionalResults[0] == "ahy" ) // should be ahy ////////////////////////////////////////
+    {
+        // misty.Debug(data.AdditionalResults[0]+" "+(data.AdditionalResults[1]).toString())
+        var headYaw = data.AdditionalResults[1];//*180.0/Math.PI;
+        headYaw = headYaw <-45.0 ? -45.0 : headYaw;
+        headYaw = headYaw >45.0 ? 45.0 : headYaw;
+        misty.Set("headYaw", headYaw);
+    }
+}
+
+// ------------------------------------ Robot Heading Updater --------------------------------------------
+
+function registerIMU() 
+{
+	misty.RegisterEvent("Heading", "IMU",100 ,true); 
+}
+
+function _Heading(data)
+{   
+    var yaw = data.PropertyTestResults[0].PropertyParent.Yaw;//*180.0/Math.PI;
+    // yaw *= -1; // CW to CCW
+    if (yaw > 180) yaw -= 360;
+    misty.Set("robotYaw", yaw);
+    // misty.Debug(yaw.toString()+" <-- Yaw");
+}
+
+// ----------------------------------- Support Functions ------------------------------------------
+
+function AngleDifference(now, to)
+{
+    var diff = ( to - now + 180 ) % 360 - 180;
+    return diff < -180 ? diff + 360 : diff;
+}
+
+function secondsPast(value)
+{
+	var timeElapsed = new Date() - new Date(value);
+    timeElapsed /= 1000;
+    return timeElapsed; // seconds
+}
+
+function toRobotFrame(data)
+{
+    var soundIn = 360-data;
+    if (soundIn>180) soundIn -= 360;
+    return (soundIn);//-1*(data-180)
+    // return data;
+}
+
+function offset_heading(to_offset){
+	var heading = misty.Get("robotYaw")+to_offset;
+	return (360.0+(heading%360))%360.0;
+}
+
+
---
+++ ExampleSkills/Intermediate/compliantHead/compliance.js
@@ -0,0 +1,119 @@
+// This skill makes Mistys head compliant!
+
+registerActuatorPosition();
+
+registerCapTouch();
+
+misty.Set("Chin", false);
+misty.Set("HeadRight", false);
+misty.Set("HeadLeft", false);
+misty.Set("HeadFront", false);
+misty.Set("HeadBack", false);
+misty.Set("Scruff", false);
+misty.Set("inTouch", false);
+misty.Set("roll", 0.0);
+misty.Set("pitch", 0.0); 
+misty.Set("yaw", 0.0);
+
+// Event to handle any Capacitive Touch Event
+function registerCapTouch(){
+
+    misty.AddReturnProperty("Touched", "sensorPosition");
+    misty.AddReturnProperty("Touched", "IsContacted");
+    misty.RegisterEvent("Touched", "TouchSensor", 0 ,true);
+
+}
+
+// We are disengaging Mistys head on any capTouch event trigger
+function _Touched(data)
+{
+    var sensor = data.AdditionalResults[0];
+    var isPressed = data.AdditionalResults[1];
+	isPressed ? misty.Debug(sensor+" is Touched") : misty.Debug(sensor+" is Released");
+    
+    if (isPressed )
+    {
+
+        misty.Set("inTouch", true);
+        disengage(); 
+        if (sensor == "Chin") misty.Set("Chin", true);
+        else if (sensor == "HeadRight") misty.Set("HeadRight", true);
+        else if (sensor == "HeadLeft") misty.Set("HeadLeft", true);
+        else if (sensor == "HeadFront") misty.Set("HeadFront", true);
+        else if (sensor == "HeadBack") misty.Set("HeadBack", true);
+        else if (sensor == "Scruff") misty.Set("Scruff", true);
+        else misty.Debug("Sensor Name Unknown");
+
+    } else {
+
+        if (sensor == "Chin") misty.Set("Chin", false);
+        else if (sensor == "HeadRight") misty.Set("HeadRight", false);
+        else if (sensor == "HeadLeft") misty.Set("HeadLeft", false);
+        else if (sensor == "HeadFront") misty.Set("HeadFront", false);
+        else if (sensor == "HeadBack") misty.Set("HeadBack", false);
+        else if (sensor == "Scruff") misty.Set("Scruff", false);
+        else {}   
+
+    }
+
+    // This block of code below, reengages Mistys head back to where she was looking 
+    if ( !misty.Get("Chin") && !misty.Get("HeadRight") && !misty.Get("HeadLeft") && !misty.Get("HeadFront") && !misty.Get("HeadBack") && !misty.Get("Scruff"))
+    {   
+        misty.Debug("Reengaging");
+        // Next four lines are temporary and is needed today to handle a bug in our effective action runner
+        var p = misty.Get("pitch")>=0.0 ? misty.Get("pitch")-10.0 : misty.Get("pitch")+10.0
+        var r = misty.Get("roll")>=0.0 ? misty.Get("roll")-10.0 : misty.Get("roll")+10.0;
+        var y = misty.Get("yaw")>=0.0 ? misty.Get("yaw")-10.0 : misty.Get("yaw")+10.0;
+        misty.MoveHeadDegrees(p, r, y, 300);
+        misty.Pause(100);
+        misty.MoveHeadDegrees(misty.Get("pitch"), misty.Get("roll"), misty.Get("yaw"), 300);
+        misty.Set("inTouch", false);
+    } else {}
+
+} 
+
+function disengage()
+{
+
+    misty.Halt();
+    misty.Debug("Halting");
+
+}
+
+// Event to track Misty's Head Position
+function registerActuatorPosition()
+{   
+
+    misty.AddReturnProperty("Positions", "SensorId");
+    misty.AddReturnProperty("Positions", "Value");
+    misty.RegisterEvent("Positions", "ActuatorPosition", 400 ,true); 
+
+}
+
+// Updates Misty's Head Position in the skill - Needed to reengage
+function _Positions(data) 
+{   
+    if (!misty.Get("inTouch"))
+    {
+
+        if (data.AdditionalResults[0] == "ahy" ) misty.Set("yaw", data.AdditionalResults[1]);
+        else if (data.AdditionalResults[0] == "ahp" ) misty.Set("pitch", data.AdditionalResults[1]);
+        else if (data.AdditionalResults[0] == "ahr" ) misty.Set("roll", data.AdditionalResults[1]);
+        else {}
+
+    } else {}
+}
+
+
+// Use the Below Function for Debug Purposes
+// misty.RegisterTimerEvent("test", 100, true);
+
+// function _test()
+// {
+//     if (misty.Get("Chin")) misty.Debug("CHIN ON");
+//     if (misty.Get("HeadRight")) misty.Debug("HeadRight ON");
+//     if (misty.Get("HeadLeft")) misty.Debug("HeadLeft ON");
+//     if (misty.Get("HeadFront")) misty.Debug("HeadFront ON");
+//     if (misty.Get("HeadBack")) misty.Debug("HeadBack ON");
+//     if (misty.Get("Scruff")) misty.Debug("Scruff ON");
+// }
