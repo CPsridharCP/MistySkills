32c5c6dca2a7ef81150a4152b5f4831dfd24737d
Johnathan <johnathan@mistyrobotics.com>
2019-07-10 16:54:05 -0600

    Remove Blinking code

--- mistyComesToLife/wakeUp.js
+++ mistyComesToLife/wakeUp.js
@@ -1,20 +1,8 @@
-// Blinking
-
-misty.Set("eyeMemory", "Homeostasis.png");
-function _blink_now(repeat = true) 
-{
-    misty.DisplayImage("blinkMisty.png");
-    misty.Pause(200);
-    misty.DisplayImage(misty.Get("eyeMemory"));
-    if (repeat) misty.RegisterTimerEvent("blink_now", getRandomInt(2, 8) * 1000, false);
-}
-misty.RegisterTimerEvent("blink_now", getRandomInt(2, 8) * 1000, false);
+// Head Movements
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// Head Movements
 
 function _look_around(repeat = true) 
 {
