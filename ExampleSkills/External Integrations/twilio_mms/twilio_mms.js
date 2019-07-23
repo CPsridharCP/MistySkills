49612f47cf221b1b386840acfb156cb00834b69e
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-07-23 14:29:56 -0600

    Add files via upload

---
+++ ExampleSkills/External Integrations/twilio_mms/twilio_mms.js
@@ -0,0 +1,12 @@
+var jsonBody = 
+{
+    'Body': '[••] Your Message Here!!',
+    'From': '<your-twilio-phone-number>',
+    'To': '<to-phone-number>',
+    'MediaUrl': '<image-URL>'
+};
+
+// base64 encoding of "ACCOUNT_ID:AUTH_TOKEN"
+var credentials = "QUN******************************************************************kwZg=="
+
+misty.SendExternalRequest("POST", "https://api.twilio.com/2010-04-01/Accounts/<ACCOUNT_ID>/Messages.json", "Basic", credentials, JSON.stringify(jsonBody), false, false, "", "application/x-www-form-urlencoded");
