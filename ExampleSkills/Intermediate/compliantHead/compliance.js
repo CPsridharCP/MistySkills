98daec50cf9253c93845fe5320ce740caae14194
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-07-28 18:34:20 -0600

    Update compliance.js

--- ExampleSkills/Intermediate/compliantHead/compliance.js
+++ ExampleSkills/Intermediate/compliantHead/compliance.js
@@ -1,5 +1,22 @@
-// This skill makes Mistys head compliant!
+/*
+*    Copyright 2019 Misty Robotics, Inc.
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
 
+
+// This skill makes Mistys head compliant!
+// By compliant - dis-engaging Misty's head motots on touch sensor activation 
+// re-engaging Misty's head motots on touch sensor release
 registerActuatorPosition();
 
 registerCapTouch();
