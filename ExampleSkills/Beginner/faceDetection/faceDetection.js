4266293e5ba19c67818219d676c1e7cdf6a6ad24
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-08-12 15:08:14 -0600

    Add files via upload

---
+++ ExampleSkills/Beginner/faceDetection/faceDetection.js
@@ -0,0 +1,52 @@
+/*
+ *    Copyright 2019 Misty Robotics, Inc.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+misty.Debug("Homing Head and Arms");
+misty.MoveHeadPosition(0, 0, 0, 40);
+misty.MoveArmDegrees("right", 70, 10);
+misty.Pause(50);
+misty.MoveArmDegrees("left", 70, 10);
+misty.ChangeLED(148, 0, 211);
+
+
+function registerFaceRec() 
+{
+    misty.AddPropertyTest("FaceRec", "PersonName", "exists", "", "string");
+    misty.RegisterEvent("FaceRec", "FaceRecognition", 1000, false);
+}
+
+function _FaceRec(data) 
+{
+    var faceDetected = data.PropertyTestResults[0].PropertyValue;
+
+    if (faceDetected == "unknown person") 
+    {
+        misty.ChangeLED(255, 0, 0);
+        misty.DisplayImage("e_Disgust.jpg");
+        misty.MoveArmDegrees("right", 70, 10);
+        misty.Pause(50);
+        misty.MoveArmDegrees("left", 70, 10);
+    } 
+    else if (faceDetected == "<Your Name>") 
+    {
+        misty.ChangeLED(148, 0, 211);
+        misty.DisplayImage("e_Joy.jpg");
+        misty.MoveArmDegrees("right", -80, 10);
+        misty.Pause(50);
+        misty.MoveArmDegrees("left", -80, 10);
+    } 
+}
+
+registerFaceRec();
