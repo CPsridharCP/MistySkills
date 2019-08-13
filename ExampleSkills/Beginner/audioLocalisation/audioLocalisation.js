e82c87ea8ba569c50b8230ecd98abb604b3919e0
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-08-12 21:58:15 -0600

    Add files via upload

---
+++ ExampleSkills/Beginner/audioLocalisation/audioLocalisation.js
@@ -0,0 +1,33 @@
+/*
+ *    Copyright 2019 Misty Robotics, Inc.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+misty.ChangeLED(0, 0, 255);
+misty.MoveHeadDegrees(-15.0, 0.0, 0.0, 70);
+misty.StartRecordingAudio("deleteThis.wav");
+misty.Pause(3000);
+misty.ChangeLED(0, 255, 0);
+
+registerAudioLocalisation();
+
+function registerAudioLocalisation() 
+{
+    misty.AddReturnProperty("soundIn", "DegreeOfArrivalSpeech");
+    misty.RegisterEvent("soundIn", "SourceTrackDataMessage", 100, true);
+}
+
+function _soundIn(data) 
+{
+    misty.Debug(data.AdditionalResults[0].toString() + " <- Audio in Degrees");
+}
