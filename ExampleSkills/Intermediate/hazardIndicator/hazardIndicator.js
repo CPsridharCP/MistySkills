a5003922f3586a2c4c109df8901112226e08eb54
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-08-29 21:06:46 -0600

    Update hazardIndicator.js

--- ExampleSkills/Intermediate/hazardIndicator/hazardIndicator.js
+++ ExampleSkills/Intermediate/hazardIndicator/hazardIndicator.js
@@ -63,3 +63,51 @@ function _Hazard(data)
     // misty.Debug(safe);
     // misty.Debug(triggers);
 }
+
+// Each Message Looks like this
+// Start--------------------
+
+// BumpSensorsHazardState: Array(4)
+// 0: {SensorName: “Bump_FrontRight”, InHazard: false}
+// 1: {SensorName: “Bump_FrontLeft”, InHazard: false}
+// 2: {SensorName: “Bump_RearRight”, InHazard: false}
+// 3: {SensorName: “Bump_RearLeft”, InHazard: false}
+
+// CriticalInternalError: 0
+
+// CurrentSensorsHazard: Array(7)
+// 0: {SensorName: “CurrentSensor_RightTrack”, InHazard: false}
+// 1: {SensorName: “CurrentSensor_LeftTrack”, InHazard: false}
+// 2: {SensorName: “CurrentSensor_RightArm”, InHazard: false}
+// 3: {SensorName: “CurrentSensor_LeftArm”, InHazard: false}
+// 4: {SensorName: “CurrentSensor_HeadPitch”, InHazard: false}
+// 5: {SensorName: “CurrentSensor_HeadRoll”, InHazard: false}
+// 6: {SensorName: “CurrentSensor_HeadYaw”, InHazard: false}
+
+// DriveStopped: Array(6)
+// 0: {SensorName: “Front right hazard”, InHazard: false}
+// 1: {SensorName: “Front center hazard”, InHazard: false}
+// 2: {SensorName: “Front left hazard”, InHazard: false}
+// 3: {SensorName: “Back right hazard”, InHazard: false}
+// 4: {SensorName: “Back center hazard”, InHazard: true}
+// 5: {SensorName: “Back left hazard”, InHazard: false}
+
+// MotorStallHazard: Array(7)
+// 0: {SensorName: “RightDrive”, InHazard: false}
+// 1: {SensorName: “LeftDrive”, InHazard: false}
+// 2: {SensorName: “RightArm”, InHazard: false}
+// 3: {SensorName: “LeftArm”, InHazard: false}
+// 4: {SensorName: “HeadPitch”, InHazard: false}
+// 5: {SensorName: “HeadRoll”, InHazard: false}
+// 6: {SensorName: “HeadYaw”, InHazard: false}
+
+// TimeOfFlightSensorsHazardState: Array(8)
+// 0: {SensorName: “TOF_Right”, InHazard: false}
+// 1: {SensorName: “TOF_Center”, InHazard: false}
+// 2: {SensorName: “TOF_Left”, InHazard: false}
+// 3: {SensorName: “TOF_Back”, InHazard: true}
+// 4: {SensorName: “TOF_DownFrontRight”, InHazard: false}
+// 5: {SensorName: “TOF_DownFrontLeft”, InHazard: false}
+// 6: {SensorName: “TOF_DownBackRight”, InHazard: false}
+// 7: {SensorName: “TOF_DownBackLeft”, InHazard: false}
+// End--------------------
