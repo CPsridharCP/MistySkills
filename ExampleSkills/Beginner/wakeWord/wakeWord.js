2c6f533de3b02c398aae532cc49ffbcfe691962c
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2020-03-03 09:17:56 -0700

    Updating Beginner WakeWord skill

--- ExampleSkills/Beginner/wakeWord/wakeWord.js
+++ ExampleSkills/Beginner/wakeWord/wakeWord.js
@@ -1,5 +1,5 @@
 /*
-*    Copyright 2019 Misty Robotics, Inc.
+*    Copyright 2020 Misty Robotics, Inc.
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
@@ -14,18 +14,26 @@
 */
 
 // Misty plays an audio on hearing "Hey Misty"
+// Once Hey Misty is triggered you would need to start it again by calling misty.StartKeyPhraseRecognition(<parameters>);
 
 detectWakeWord();
 
 function detectWakeWord() 
 {
-    misty.StartKeyPhraseRecognition();
+    // False indicated we are not recording the audio after Hey Misty is receognized
+    misty.StartKeyPhraseRecognition(false);
+
+    // Listening to the Event "KeyphraseRecognized" to take action when Hey Misty is recognized
+    // First parameter is the name of the callback function that will be triggered when the event happens (can change)
+    // Second parameter is the eventName (fixed)
+    // Third parameter is the debounce (can change)
+    // Fourth parameter is a boolean that indicates if you want to keep listening to the event trigger continuously
     misty.RegisterEvent("heyMisty", "KeyphraseRecognized", 10, false);
 }
 
 function _heyMisty() 
 {
-    misty.PlayAudio("s_SystemWakeWord.wav", 100);
+    // misty.PlayAudio("s_SystemWakeWord.wav", 100);
     misty.Debug("Wake Work Recognised");
     detectWakeWord();
 }
